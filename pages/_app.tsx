63b80fe1c1657679d26e305552bc2ed73e56dd7b
Isaac Alberto Coronel Torales <isaac.coronel@bancontinental.com.py>
2024-07-31 12:47:25 -0400

    commit 6
    commit 6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,5 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
   //lol
-  //lol123456
-  //lol1122
-  //lol
-  //lolololo
+  
 }
